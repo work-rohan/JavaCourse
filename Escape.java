a73496021a4fbfc433229e3de29753b03c01a52a
work-rohan <164131185+work-rohan@users.noreply.github.com>
2024-04-22 17:54:55 +0530

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,12 @@
+public class Escape {
+    public static void main(String[] args) {
+        String msg = "Good Evening \"Bruce\"";
+        System.out.println(msg);
+
+        String path = "c:\\User\\Downloads\\..";
+        System.out.println(path);
+
+        String newLine = "Good \t Evening \nBruce";
+        System.out.println(newLine);
+    }
+}
